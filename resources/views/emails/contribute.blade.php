ba1814c83cea1fc19f2b9a8473d36868c5a3c303
Unknown <sebastian@haraldsson.it>
2018-01-28 16:15:16 +0100

    Corrected swish amount in order mail.

--- resources/views/emails/contribute.blade.php
+++ resources/views/emails/contribute.blade.php
@@ -1,5 +1,5 @@
 <h1>Tack för din beställning</h1>
-<p>Vi har tagit emot din beställning och kommer att skicka ut den så fort vi bekräftat att du swishat in {{ 149*$request->count37*$request->count40 }}kr till <b>1234965679</b>.</p>
+<p>Vi har tagit emot din beställning och kommer att skicka ut den så fort vi bekräftat att du swishat in {{ (149*$request->count37)+(149*$request->count40) }}kr till <b>1234965679</b>.</p>
 <p>Du har beställt:</p>
 <p>{{ $request->count37 }}st strumpor i storlek 37-40.</p>
 <p>{{ $request->count40 }}st strumpor i storlek 40-45.</p>
